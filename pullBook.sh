d3c5ec48cdcfd36c45d5d6a35389cccdf23d4d36
kawsing <kawsing@gmail.com>
2019-12-23 15:36:09 +0800

    Publish Book 一 12月 23 15:36:09 CST 2019

--- pullBook.sh
+++ pullBook.sh
@@ -1,5 +1,8 @@
 #!/bin/bash 
 #author:kawsing
+#git pull origin gh-pages
+#From https://github.com/kawsing/Book
+# * branch            gh-pages   -> FETCH_HEAD => HEAD 是 gh-pages
 git init
 git remote add origin https://github.com/kawsing/Book.git
 git pull origin gh-pages
--- upmyBook.sh
+++ upmyBook.sh
@@ -2,4 +2,5 @@
 #author:kawsing
 git add .
 git commit -m "Publish Book `date`"
-git push -u origin gh-pages
+#git push -u origin gh-pages
+git push origin HEAD:gh-pages
